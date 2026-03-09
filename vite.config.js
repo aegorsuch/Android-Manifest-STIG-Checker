6a69cf74a23f29c2b9ad13a8d8701dd4a8d39981
aegorsuch <alex@weartak.com>
2026-03-09 15:07:48 -0400

    Fix: update base path in Vite configuration to match repository name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/android-manifest-stig-checker/',
+  base: '/Android-Manifest-STIG-Checker/',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
